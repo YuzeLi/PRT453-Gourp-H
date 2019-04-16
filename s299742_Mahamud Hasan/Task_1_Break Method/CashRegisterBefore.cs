8bccee49acc41f21f9e9584c9553d00a5d132ecc
Mahamud Hasan <S299742@students.cdu.edu.au>
2019-04-16 18:11:42 +0930

    Add an example that can be refactored by Break Method Remove some unused file

---
+++ s299742_Mahamud Hasan/Task_1_Break Method/CashRegisterBefore.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreakMethod_Before
+{
+    public class CashRegisterBefore
+    {
+        public CashRegisterBefore()
+        {
+            Tax = 0.06m;
+        }
+
+        private decimal Tax { get; set; }
+
+        public void AcceptPayment(Customer customer, IEnumerable<Product> products, decimal payment)
+        {
+            decimal subTotal = 0m;
+            foreach (Product product in products)
+            {
+                subTotal += product.Price;
+            }
+            foreach (Product product in products)
+            {
+                subTotal -= product.AvailableDiscounts;
+            }
+            decimal grandTotal = subTotal * Tax;
+            customer.DeductFromAccountBalance(grandTotal);
+        }
+    }
+
+    public class Customer
+    {
+        public void DeductFromAccountBalance(decimal amount)
+        {
+            // deduct from balance
+        }
+    }
+
+    public class Product
+    {
+        public decimal Price { get; set; }
+        public decimal AvailableDiscounts { get; set; }
+    }
+
+}
