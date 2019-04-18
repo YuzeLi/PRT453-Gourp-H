5803105d1475e85ab0a795a9fd1c8e281b5d6add
YuzeLi <rex6515@gmail.com>
2019-04-18 17:59:40 +0930

    Task5

---
+++ s306890 Yuze Li/Task5/Before/before.java
@@ -0,0 +1,23 @@
+   public class ClassRegistration
+      {
+          public void Create()
+          {
+              // create registration code
+          }
+  
+          public void Transfer()
+          {
+             // class transfer code
+         }
+ 
+         public decimal Total { get; private set; }
+     }
+ 
+     public class RegistrationProcessor
+     {
+         public decimal ProcessRegistration(ClassRegistration registration)
+         {
+             registration.Create();
+             return registration.Total;
+         }
+     }
