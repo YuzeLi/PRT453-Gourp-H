c9f82bc2692316cbd8de1fd65ca0513436768ae7
Mahamud Hasan <S299742@students.cdu.edu.au>
2019-04-25 08:54:13 +0930

    Working on Task_5. MOdify and refactoring it.

---
+++ s299742_Mahamud Hasan/Task_5_Introduce Parameter Object/Before.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroduceParameterObject
+{
+    class Account
+    {
+        private List<Entry> lstEntries { get; set; }
+
+        public double GetResult(DateTime start, DateTime end)
+        {
+            double result = 0;
+            foreach (var entry in lstEntries)
+            {
+                if (start <= entry.ChargeDate && end >= entry.ChargeDate)
+                {
+                    result += entry.value;
+                }
+            }
+            return result;
+        }
+    }
+
+    internal class Entry
+    {
+        internal double value;
+
+        public DateTime ChargeDate { get; internal set; }
+    }
+}
