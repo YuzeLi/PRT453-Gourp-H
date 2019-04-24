d71aeb18a92bddd9d0c8df0cfb2f3cc43b51aa0f
Mahamud Hasan <S299742@students.cdu.edu.au>
2019-04-25 08:24:05 +0930

    Complete the task 4. Write the before and after code. Write down the refactoring process.

---
+++ s299742_Mahamud Hasan/Task_4_Remove Double Negetive/After.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleNegative
+{
+    public class Order
+    {
+        public void Checkout(IEnumerable<Product> products, Customer customer)
+        {
+            if (customer.IsFlagged)
+            {
+                return;
+            }
+        }
+    }
+
+    public class Customer
+    {
+        public decimal Balance { get; private set; }
+
+        public bool IsFlagged
+        {
+            get { return Balance >= 30m; }
+        }
+    }
+
+    public class Product
+    {
+    }
+}
---
+++ s299742_Mahamud Hasan/Task_4_Remove Double Negetive/Before.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleNegative
+{
+    public class Order
+    {
+        public void Checkout(IEnumerable<Product> products, Customer customer)
+        {
+            if (!customer.IsNotFlagged)
+            {
+                return;
+            }
+        }
+    }
+    public class Customer
+    {
+        public decimal Balance { get; private set; }
+        public bool IsNotFlagged
+        {
+            get { return Balance < 30m; }
+        }
+    }
+    public class Product
+    {
+    }
+}
