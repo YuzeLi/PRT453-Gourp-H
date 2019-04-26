3e6403548e7ad8a21b6c47003295e453dd506265
Mahamud Hasan <S299742@students.cdu.edu.au>
2019-04-26 13:41:26 +0930

    Add new task to project backlog for sprint 2

---
+++ s299742_Mahamud Hasan/Task_9_Remove Duplication/Before.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtractSubClass
+{
+    class JobItem
+    {
+        public int Quantity { get; private set; }
+        public Employee Employee { get; private set; }
+        public bool IsLabour { get; set; }
+        private int _unitPrice;
+
+        public int UnitPrice
+        {
+            get
+            {
+                return IsLabour ? Employee.Rate : UnitPrice;
+            }
+            set
+            {
+                _unitPrice = value;
+            }
+        }
+
+        public int TotalPrice
+        {
+            get { return UnitPrice * Quantity; }
+        }
+
+        public JobItem(int unitPrice, int quantity, bool isLabour, Employee employee)
+        {
+            UnitPrice = unitPrice;
+            Quantity = quantity;
+            IsLabour = isLabour;
+            Employee = employee;
+        }
+
+    }
+
+    class Employee
+    {
+        public int Rate { get; internal set; }
+    }
+
+
+}
