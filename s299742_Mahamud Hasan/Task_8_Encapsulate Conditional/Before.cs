0a34546d8f73e37e5553f395c793b3c5c4408f7d
Mahamud Hasan <S299742@students.cdu.edu.au>
2019-04-27 23:30:47 +0930

    Add next task

--- s299742_Mahamud Hasan/Task_8_Encapsulate Conditional/Before.cs
+++ s299742_Mahamud Hasan/Task_8_Encapsulate Conditional/Before.cs
@@ -6,19 +6,18 @@ using System.Threading.Tasks;
 
 namespace EncapsulateConditional
 {
-   1: public class RemoteControl
-2: {
-3: private string[] Functions { get; set; }
-4: private string Name { get; set; }
-5: private int CreatedYear { get; set; }
-6:
-7: public string PerformCoolFunction(string buttonPressed)
-8: {
-9: // Determine if we are controlling some extra function
-10: // that requires special conditions
-11: if (Functions.Length > 1 && Name == "RCA" &&
-CreatedYear > DateTime.Now.Year - 2)
-12: return "doSomething";
-13: }
-14: }
+public class RemoteControl
+{
+private string[] Functions { get; set; }
+private string Name { get; set; }
+private int CreatedYear { get; set; }
+
+public string PerformCoolFunction(string buttonPressed)
+{
+// Determine if we are controlling some extra function
+ // that requires special conditions
+ if (Functions.Length > 1 && Name == "RCA" && CreatedYear > DateTime.Now.Year - 2)
+ return "doSomething";
+ }
+ }
 }
--- s299742_Mahamud Hasan/Task_9_Remove Duplication/Before.cs
+++ s299742_Mahamud Hasan/Task_9_Remove Duplication/Before.cs
@@ -4,46 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ExtractSubClass
+namespace RemoveDuplication
 {
-    class JobItem
-    {
-        public int Quantity { get; private set; }
-        public Employee Employee { get; private set; }
-        public bool IsLabour { get; set; }
-        private int _unitPrice;
-
-        public int UnitPrice
-        {
-            get
-            {
-                return IsLabour ? Employee.Rate : UnitPrice;
-            }
-            set
-            {
-                _unitPrice = value;
-            }
-        }
-
-        public int TotalPrice
-        {
-            get { return UnitPrice * Quantity; }
-        }
-
-        public JobItem(int unitPrice, int quantity, bool isLabour, Employee employee)
-        {
-            UnitPrice = unitPrice;
-            Quantity = quantity;
-            IsLabour = isLabour;
-            Employee = employee;
-        }
-
-    }
-
-    class Employee
-    {
-        public int Rate { get; internal set; }
-    }
+   
 
 
 }
