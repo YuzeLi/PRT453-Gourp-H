1f5f31544c0747910b137954ff9701b517176b74
Mahamud Hasan <S299742@students.cdu.edu.au>
2019-05-08 03:05:33 +0930

    Add before code and diagram for Sprint_2 and Task_8. Write refactored code and refactoring process

---
+++ s299742_Mahamud Hasan/Task_8_Extract Interface/After.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tsk_8_EncpslteConditional
+{
+    class TimeSheet
+    {
+        public IBillable Resources { get; private set; }
+        public double GetCharge(int days)
+        {
+            double result = Resources.Rate * days;
+            if (Resources.HasSpecialSkill)
+            {
+                result *= 1.05;
+            }
+            return result;
+        }
+    }
+    interface IBillable
+    {
+        double Rate { get; }
+        bool HasSpecialSkill { get; }
+    }
+    public class Employee:IBillable
+    {
+        public double Rate { get; private set; }
+        public bool HasSpecialSkill { get; private set; }
+    }
+}
---
+++ s299742_Mahamud Hasan/Task_8_Extract Interface/Before.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tsk_8_EncpslteConditional
+{
+    class TimeSheet
+    {
+        public Employee employee { get; private set; }
+
+        public double GetCharge(int days)
+        {
+            double result = employee.Rate * days;
+            if (employee.HasSpecialSkill)
+            {
+                result *= 1.05;
+            }
+
+            return result;
+        }
+    }
+
+
+
+    public class Employee
+    {
+        public int Rate { get; private set; }
+        public bool HasSpecialSkill { get; private set; }
+    }
+}
