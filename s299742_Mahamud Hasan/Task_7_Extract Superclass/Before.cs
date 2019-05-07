1e3d57b80da8ae05c57e4a42a84d85cbc853f093
Mahamud Hasan <S299742@students.cdu.edu.au>
2019-05-07 21:37:50 +0930

    Working on Task 7 for sprint two. Add Before code for refactoring super class

--- s299742_Mahamud Hasan/Task_7_Extract Superclass/Before.cs
+++ s299742_Mahamud Hasan/Task_7_Extract Superclass/Before.cs
@@ -4,19 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ExtractSuperClass
+namespace Task_7_ExtractSuperClass
 {
- public class Dog
-{
-public void EatFood()
-{
-// eat some food
-}
+    class Employee
+    {
+        public string Name { get; private set; }
+        public string ID { get; private set; }
+        public int AnnualCost { get; private set; }
 
-public void Groom()
-{
- // perform grooming
- }
- }
-
+        public Employee(string name, string id, int cost)
+        {
+            Name = name;
+            ID = id;
+            AnnualCost = cost;
+        }
+    }
+    class Department
+    {
+        public string Name { get; private set; }
+        public IEnumerable<Employee> Staff { get; private set; }
+        public int TotalAnnualCost
+        {
+            get { return Staff.Sum(s => s.AnnualCost); }
+        }
+        public int HeadCount
+        {
+            get { return Staff.Count(); }
+        }
+        public Department(string name)
+        {
+            Name = name;
+            Staff = new List<Employee>();
+        }
+        public void AddStaff(Employee employee)
+        {
+            ((IList<Employee>)Staff).Add(employee);
+        }
+    }
 }
