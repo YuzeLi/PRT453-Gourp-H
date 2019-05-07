19854db14870e6c148b0fc6eb854ef7dc6102cc4
Mahamud Hasan <S299742@students.cdu.edu.au>
2019-05-08 05:18:00 +0930

    Add Before code, After code, Refactoring Process as part of sprint 2

--- s299742_Mahamud Hasan/Task_9_Remove Duplication/After.cs
+++ s299742_Mahamud Hasan/Task_9_Remove Duplication/After.cs
@@ -1,10 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RemoveDuplication
+namespace Task_9_RemoveDuplication
 {
-    
+    public abstract class Duplications
+    {
+        public abstract void saveFile(String filename, String fileContent);
+        public abstract String readFile(String filename);
+        public abstract void log(String level, String message);
+
+        // boiler plate code
+        private void ExecuteWithNiceLogging(Closure algorithm, String errorMessage)
+        {
+            try
+            {
+                algorithm.call();
+            }
+            catch (IOException error)
+            {
+                // please provide better error diagnosis in real life!
+                log("Error", errorMessage);
+            }
+        }
+
+        // code interesting to business
+        public void createFile()
+        {
+            saveGreetings("Beyond Java says \"Hello World!\"");
+        }
+        // code interesting to business
+        private void saveGreetings(String fileContents)
+        {
+           executeWithNiceLogging(saveFile("Greetings.txt", fileContents), "Can't write the file due to an exception");
+        }
+
+
+        // code interesting to business
+        public void copyFile()
+        {
+            executeWithNiceLogging({ saveGreetings(readFile("OldFileName.txt")) }, "Can't read the file")
+        }
+    }
 }
--- s299742_Mahamud Hasan/Task_9_Remove Duplication/Before.cs
+++ s299742_Mahamud Hasan/Task_9_Remove Duplication/Before.cs
@@ -1,12 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RemoveDuplication
+namespace Task_9_RemoveDuplication
 {
-   
-
-
+    public abstract class Duplications
+    {
+        public abstract void saveFile(String filename, String fileContent);
+        public abstract String readFile(String filename);
+        public abstract void log(String level, String message);
+        public void createFile()
+        {
+            try
+            {
+                this.saveFile("Greetings.txt", "Beyond Java says \\\"Hello World!\\\"\")");
+            }
+            catch (IOException error)
+            {
+                //  please provide better error diagnosis in real life!
+                this.log("Error", "Can\'t write the file due to an exception");
+            }
+        }
+        public void copyFile()
+        {
+            try
+            {
+                String fileContents = this.readFile("OldFileName.txt");
+                try
+                {
+                    this.saveFile("Greetings.txt", fileContents);
+                }
+                catch (IOException error)
+                {
+                    //  please provide better error diagnosis in real life!
+                    this.log("Error", "Can\'t write the file due to an exception");
+                }
+            }
+            catch (IOException error)
+            {
+                //  please provide better error diagnosis in real life!
+                this.log("Error", "Can\'t read the file");
+            }
+        }
+    }
 }
