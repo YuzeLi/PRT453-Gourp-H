ebc2cda97b81e811619b4022b9b88086665cb15e
Mahamud Hasan <S299742@students.cdu.edu.au>
2019-05-08 06:25:53 +0930

    Complete Task 10 for sprint 2

--- s299742_Mahamud Hasan/Task_10_Extract Method Object/Before.cs
+++ s299742_Mahamud Hasan/Task_10_Extract Method Object/Before.cs
@@ -1,11 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ExtractMethodObject
+namespace Task_10_ExtrctMethodObject
 {
-    
+    public class OrderLineItem
+    {
+        public decimal Price { get; private set; }
+    }
 
+    public class Order
+    {
+        private IList<OrderLineItem> OrderLineItems { get; set; }
+        private IList<decimal> Discounts { get; set; }
+        private decimal Tax { get; set; }
+
+        public decimal Calculate()
+        {
+            decimal subTotal = 0m;
+
+            // Total up line items
+            foreach (OrderLineItem lineItem in OrderLineItems)
+            {
+                subTotal += lineItem.Price;
+            }
+
+            // Subtract Discounts
+            foreach (decimal discount in Discounts)
+                subTotal -= discount;
+
+            // Calculate Tax
+            decimal tax = subTotal * Tax;
+
+            // Calculate GrandTotal
+            decimal grandTotal = subTotal + tax;
+
+            return grandTotal;
+        }
+    }
 }
