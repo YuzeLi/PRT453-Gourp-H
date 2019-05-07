36eb257687df5fef5101d6ad2985546ff21d34c2
Mahamud Hasan <S299742@students.cdu.edu.au>
2019-05-07 21:39:05 +0930

    Write refactored code, user story and refactoring process for task 7 as sprint 2

--- s299742_Mahamud Hasan/Task_7_Extract Superclass/After.cs
+++ s299742_Mahamud Hasan/Task_7_Extract Superclass/After.cs
@@ -4,7 +4,56 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ExtractSubClass
+namespace Task_7_ExtractSuperClass
 {
-    
+    /// <summary>
+    /// SuperClass
+    /// </summary>
+    abstract class Party
+    {
+        public string Name { get; private set; }
+        public abstract int AnnualCost { get; }
+        protected Party(string name)
+        {
+            Name = name;
+        }
+    }
+
+    class Employee : Party
+    {
+        public string ID { get; private set; }
+        private int _annualCost;
+        public override int AnnualCost
+        {
+            get { return _annualCost; }
+        }
+        public Employee(string name, string id, int cost)
+        : base(name)
+        {
+            ID = id;
+            _annualCost = cost;
+        }
+    }
+
+    class Department : Party
+    {
+        public IEnumerable<Party> Parties { get; private set; }
+        public override int AnnualCost
+        {
+            get { return Parties.Sum(s => s.AnnualCost); }
+        }
+        public int HeadCount
+        {
+            get { return Parties.Count(); }
+        }
+        public Department(string name)
+            : base(name)
+        {
+            Parties = new List<Party>();
+        }
+        public void AddParty(Party party)
+        {
+            ((IList<Party>)Parties).Add(party);
+        }
+    }
 }
