6a7c88ce3a87e005b8ffbd358ebc3c4ebcc89bbb
YuzeLi <rex6515@gmail.com>
2019-05-09 18:07:20 +0930

    task 7 - 9 after

---
+++ s306890 Yuze Li/Task8/After/after.cs
@@ -0,0 +1,15 @@
+public abstract class Animal
+ {
+ }
+
+public class Dog : Animal
+ {
+ 	public void Bark()
+ 	{
+ 		// code to bark
+ 	}
+ }
+
+public class Cat : Animal
+ {
+ }
---
+++ s306890 Yuze Li/Task9/After/after.cs
@@ -0,0 +1,11 @@
+// Changed the class name to Employee
+public class Employee
+ {
+		 public string FirstName { get; set; }
+
+		 public decimal CalculateHourlyPay()
+			 {
+				 // code to calculate hourly payrate
+				 return 0m;
+			 }
+ } 
